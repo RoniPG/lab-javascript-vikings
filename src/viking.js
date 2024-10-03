43e71b992912a7f5a30c7d31724aeda583bf5ea4
RoniPG <ronigmn@gmail.com>
2024-10-03 20:27:32 +0200

    Iteration 1: Soldier

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,15 @@
 // Soldier
 class Soldier {
-    constructor(health, strength) {
-        
+    constructor (health, strength) {   
+        this.health = health;
+        this.strength= strength;
     }
+   attack () {
+    return this.strength;
+   }
+   receiveDamage (damage) {
+    this.health = this.health - damage;
+   }
 }
 
 // Viking
